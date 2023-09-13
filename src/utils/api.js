b5f5d129fbb67c3efe748434e6c5da110e4aa8ef
lavikara <karatemitayo@gmail.com>
2023-09-13 22:00:06 +0100

    fixed error

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,16 +4,7 @@ const baseUrl = `https://52fa0596-5e91-4e7c-b2a8-bb3d4b8b66c7.mock.pstmn.io`;
 const paymentUrl = `${baseUrl}//woodcore/payment`;
 
 export default {
-  getHeader: () => {
-    let headers = {
-      Authorization: "Bearer " + getItem("org_token"),
-    };
-    return headers;
-  },
-
   getTransactions(id) {
-    return axios.get(`${paymentUrl}/${id}`, {
-      headers: this.getHeader(),
-    });
+    return axios.get(`${paymentUrl}/${id}`, {});
   },
 };
